54190180960159dfef5fec021f35b3adda656d09
Angus <angus.ferraro@googlemail.com>
2015-11-17 10:23:51 +0000

    Added saturation vapour pressure function to thermodynamics

--- jazz/thermodynamics.py
+++ jazz/thermodynamics.py
@@ -26,4 +26,22 @@ def dry_adiabat():
 
 
 def svp(ta):
-    pass
+    """Calculate saturation vapour pressure using the
+    August-Roche-Magnus formula (Lawrence 2005).
+
+    Args:
+        ta: temperature in Kelvin
+
+    References:
+        Lawrence, M. G., 2005: The relationship between relative
+            humidity and the dewpoint temperature in moist air: A simple
+            conversion and applications. Bull. Am. Meteorol. Soc., 86,
+            225–233, doi:10.1175/BAMS-86-2-225.
+
+    """
+    # Convert to degrees C
+    ta = ta - 273.15
+    c1 = 610.94 # Pa
+    a1 = 17.625
+    b1 = 243.04 # C
+    return c1 * np.exp(a1*t/(b1+t))
